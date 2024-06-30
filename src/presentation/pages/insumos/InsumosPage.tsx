798fc980cb60e231b0a33742a1afb951a55b9ed9
Jesus Reyes <luisdejesus200122@gmail.com>
2024-06-29 18:00:36 -0600

    .

--- src/presentation/pages/insumos/InsumosPage.tsx
+++ src/presentation/pages/insumos/InsumosPage.tsx
@@ -1,7 +1,27 @@
+import { CustomButton } from "../../components/shared/button/CustomButton";
 
  const InsumosPage = () => {
   return (
-    <div>InsumosPage</div>
+    <>
+    <div className="home-container">
+        <h1 className="py-4 ml-3 mt-3 text-2xl font-semibold">
+          Gestion de Insumos
+        </h1>
+        <div className="flex  w-full items-end justify-end ">
+          <div className="mr-5 ">
+            <CustomButton
+              title={'Agregar Insumo al Inventario'}
+              onClick={()  => console.log('click')}
+            />
+          </div>
+        </div>
+        {/* <CustomTableComponent
+          isLoading={status === Status.inProgress}
+          items={inventarioResponse ?? []}
+        /> */}
+      </div>
+    
+    </>
   )
 }
 
